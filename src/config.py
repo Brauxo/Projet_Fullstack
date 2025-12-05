c1f3090d2f0a619ddf8f9d047993196a2a780b21
Brauxo <owen.braux@gmail.com>
2025-12-05 13:52:15 +0100

    fix database error for CI/CD

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '3aef3dbe0736d57875c1b25840e30c46e9a460ced44aa6c6f1da2ced83d8e910'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@db:5432/forum_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@db:5432/forum_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RAWG_API_KEY = os.environ.get('RAWG_API_KEY')
